c3b2ccb3e8274c21c809d4ae8a4c9ac197fb0c23
Arthur Brasil <91575015+ArthurMarcioBrasil@users.noreply.github.com>
2021-11-17 11:27:01 -0300

    Add files via upload

--- spring/agenda/src/main/java/api/agenda/model/Pessoa.java
+++ spring/agenda/src/main/java/api/agenda/model/Pessoa.java
@@ -7,8 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
-
 
 
 @Entity
